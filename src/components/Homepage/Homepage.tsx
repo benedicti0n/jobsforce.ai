277e32aeab980800e38e851612e19354dc30d486
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-03-11 02:34:43 +0530

    feat: add Upload component for file upload functionality and update styles across Homepage and Navbar

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -8,6 +8,7 @@ import { BriefcaseBusiness, Users } from 'lucide-react';
 import { AvatarCircles } from '../ui/magicui/avatar-circles';
 import LogoBox from '../ui/LogoBox';
 import { avatars, rightLogos, leftLogos } from './data';
+import Upload from '../ui/Upload';
 
 const Homepage = () => {
     return (
@@ -22,7 +23,7 @@ const Homepage = () => {
                 refresh
             />
 
-            <div className='w-2/3 flex flex-col items-center justify-center z-50 text-main relative'>
+            <div className='w-2/3 flex flex-col items-center justify-center z-50 text-main relative mt-16'>
                 <div className='h-full absolute left-0 space-y-20'>
                     {rightLogos.map((logo) => (
                         <LogoBox key={logo.key} rotate={logo.rotate} position={logo.position}>
@@ -38,57 +39,75 @@ const Homepage = () => {
                     ))}
                 </div>
 
-                <div className='flex gap-16 font-thin'>
+                <div className='flex gap-16 font-regular'>
                     <ShimmerButton
                         className='text-secondary gap-2'
                         shimmerColor='#EFBF04'
-                        shimmerDuration='3s'
+                        shimmerDuration='5s'
                         background='#000'
                     >
-                        <Users className='w-5 stroke-1' />
+                        <Users className='w-5 stroke-2' />
                         900+ Job Seeker Helped
                     </ShimmerButton>
 
                     <ShimmerButton
                         className='text-secondary gap-2'
                         shimmerColor='#EFBF04'
-                        shimmerDuration='3s'
+                        shimmerDuration='5s'
                         background='#000'
                     >
-                        <BriefcaseBusiness className='w-5 stroke-1' />
+                        <BriefcaseBusiness className='w-5 stroke-2' />
                         500k+ Jobs Applied
                     </ShimmerButton>
                 </div>
                 <div className='flex flex-col items-center justify-center mt-8'>
-                    <h1 className='text-5xl font-regular'>We will c🍳🧑‍🍳k up the</h1>
-                    <h1 className='text-7xl font-bold flex gap-4'>
-                        <SparklesText text='Perfect recipe' className=' text-7xl' colors={{ first: "#FF8C32", second: "#EFBF04" }} sparklesCount={6} />
+                    <h1 className='md:text-4xl font-regular'>We will c🍳🧑‍🍳k up the</h1>
+                    <h1 className='md:text-6xl font-bold flex gap-4'>
+                        <SparklesText text='Perfect recipe' className=' md:text-6xl' colors={{ first: "#FF8C32", second: "#EFBF04" }} sparklesCount={6} />
                         for you
                     </h1>
                 </div>
                 <div className='flex flex-col justify-center items-center gap-2 mt-10'>
-                    <h1 className='text-xl font-regular text-main'>Loved By 100k+ People</h1>
-                    <AvatarCircles numPeople={99} avatarUrls={avatars} />;
+                    <h1 className='md:text-lg font-regular text-main'>Loved By 100k+ People</h1>
+                    <AvatarCircles numPeople={99} avatarUrls={avatars} />
                 </div>
+
+                <Upload />
             </div>
 
-            {/* orange */}
+
             <div
                 className="h-full w-full absolute bottom-0"
-                style={{ background: "radial-gradient(150% 100% at 50% 20%, #000 50%, rgba(255, 255, 100, 1) 100%)" }}
+                style={{ background: "radial-gradient(70% 30% at 50% 110%, rgba(255, 255, 100, 1) 5%, #000 120%)" }}
+            ></div>
+
+            <div
+                className="h-full w-full absolute bottom-0 z-10 mix-blend-overlay"
+                style={{ background: "radial-gradient(70% 40% at 50% 100%, rgba(255, 140, 50, 1) 30%, #000 100%)" }}
+            ></div>
+
+            <div
+                className="h-full w-full absolute bottom-0 z-20 mix-blend-overlay"
+                style={{ background: "radial-gradient(60% 80% at 50% 130%, rgba(255, 255, 255, 1) 30%, #000 100%)" }}
             ></div>
 
             {/* yellow */}
-            <div
+            {/* <div
+                className="h-full w-full absolute bottom-0"
+                style={{ background: "radial-gradient(150% 100% at 50% 20%, #000 50%, rgba(255, 255, 100, 1) 100%)" }}
+            ></div> */}
+
+            {/* orange */}
+            {/* <div
                 className="h-full w-full absolute bottom-0 z-10 mix-blend-overlay"
                 style={{ background: "radial-gradient(120% 100% at 50% 10%, #000 50%, rgba(255, 140, 50, 1) 100%)" }}
-            ></div>
+            ></div> */}
 
             {/* white */}
-            <div
+            {/* <div
                 className="h-full w-full absolute bottom-0 z-20 mix-blend-overlay"
                 style={{ background: "radial-gradient(90% 100% at 50% 20%, #000 50%, rgba(255, 255, 255, 1) 100%)" }}
-            ></div>
+            ></div> */}
         </motion.div>
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import { Wallet } from 'lucide-react';
 import FlipText from '../ui/FlipText';
-// import { Button } from '@radix-ui/themes';
 import Button from '../ui/Button';
 import * as motion from "motion/react-client"
 
 const Navbar = () => (
     <motion.div
-        className='w-2/3 fixed z-50 top-5 flex justify-between items-center rounded-3xl p-[1px] bg-gradient-to-br from-[#FFD700]/70 to-[#FFD700]/20'
+        className='w-2/3 h-16 fixed z-60 top-5 flex justify-between items-center rounded-2xl p-[1px] bg-gradient-to-br from-[#FFD700]/70 to-[#FFD700]/20'
         initial={{ y: -100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5 }}
     >
-        <div className='w-full h-full flex justify-between items-center rounded-[1.4rem] bg-black px-6 py-4 text-main'>
-            <img src={"/logo.png"} alt='logo' className='w-10' />
+        <div className='w-full h-full flex justify-between items-center rounded-2xl bg-black px-6 py-4 text-main'>
+            <img src={"/logo.png"} alt='logo' className='h-full' />
             <div className='flex gap-6'>
                 <FlipText>Tools</FlipText>
                 <FlipText>Resources</FlipText>

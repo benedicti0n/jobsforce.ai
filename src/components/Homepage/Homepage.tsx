a8d1a275e3110cae1239e322b05a56dab2099bb6
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-03-10 07:21:54 +0530

    feat: enhance Homepage with new magic UI components and dynamic animations

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,44 +1,94 @@
 import React from 'react'
-import { useEffect } from "react";
-import { animate } from "motion";
-import { useMotionTemplate, useMotionValue } from "motion/react";
 import * as motion from "motion/react-client";
 import Navbar from "@/components/Navbar/Navbar";
-import { useAtom } from "jotai";
-import { darkModeAtom } from "@/store";
-
-const COLORS = ["#EFBF04"];
+import { SparklesText } from '../magicui/sparkles-text';
+import { Particles } from '../magicui/particles';
+import { ShimmerButton } from '../magicui/shimmer-button';
+import { BriefcaseBusiness, Users } from 'lucide-react';
+import { AvatarCircles } from '../magicui/avatar-circles';
+import LogoBox from '../ui/LogoBox';
+import { avatars, rightLogos, leftLogos } from './data';
 
 const Homepage = () => {
-    const color = useMotionValue(COLORS[0]);
-    const [isDark] = useAtom(darkModeAtom);
-
-    const backgroundImage = useMotionTemplate`
-    radial-gradient(120% 100% at 50% 20%, ${isDark ? "#000" : "#fff"} 50%, ${color} 100%)
-  `;
-
-    useEffect(() => {
-        animate(color, COLORS, {
-            ease: "easeInOut",
-            duration: 20,
-            repeat: Infinity,
-            repeatType: "mirror",
-        });
-    }, []);
-
     return (
-        <motion.div
-            className="h-screen w-full flex justify-center items-center"
-            style={{
-                backgroundImage,
-            }}
-        >
+        <motion.div className="h-screen w-full flex justify-center items-center relative">
             <Navbar />
 
-            {/* <div className='flex flex-col items-center justify-center font-sequel-medium'>
-                <h1 className='text-5xl font-bold'>We will c🍳🧑‍🍳k up the</h1>
-                <h1 className='text-7xl font-bold font'>Perfect recipe for you</h1>
-            </div> */}
+            <Particles
+                className="absolute inset-0 z-30"
+                quantity={100}
+                ease={80}
+                color={"#EFBF04"}
+                refresh
+            />
+
+            <div className='w-2/3 flex flex-col items-center justify-center z-50 text-main relative'>
+                <div className='h-full absolute left-0 space-y-20'>
+                    {rightLogos.map((logo) => (
+                        <LogoBox key={logo.key} rotate={logo.rotate} position={logo.position}>
+                            {logo.logo}
+                        </LogoBox>
+                    ))}
+                </div>
+                <div className='h-full absolute right-0 space-y-20'>
+                    {leftLogos.map((logo) => (
+                        <LogoBox key={logo.key} rotate={logo.rotate} position={logo.position}>
+                            {logo.logo}
+                        </LogoBox>
+                    ))}
+                </div>
+
+                <div className='flex gap-16 font-thin'>
+                    <ShimmerButton
+                        className='text-secondary gap-2'
+                        shimmerColor='#EFBF04'
+                        shimmerDuration='3s'
+                        background='#000'
+                    >
+                        <Users className='w-5 stroke-1' />
+                        900+ Job Seeker Helped
+                    </ShimmerButton>
+
+                    <ShimmerButton
+                        className='text-secondary gap-2'
+                        shimmerColor='#EFBF04'
+                        shimmerDuration='3s'
+                        background='#000'
+                    >
+                        <BriefcaseBusiness className='w-5 stroke-1' />
+                        500k+ Jobs Applied
+                    </ShimmerButton>
+                </div>
+                <div className='flex flex-col items-center justify-center mt-8'>
+                    <h1 className='text-5xl font-regular'>We will c🍳🧑‍🍳k up the</h1>
+                    <h1 className='text-7xl font-bold flex gap-4'>
+                        <SparklesText text='Perfect recipe' className=' text-7xl' colors={{ first: "#FF8C32", second: "#EFBF04" }} sparklesCount={10} />
+                        for you
+                    </h1>
+                </div>
+                <div className='flex flex-col justify-center items-center gap-2 mt-10'>
+                    <h1 className='text-xl font-regular text-main'>Loved By 100k+ People</h1>
+                    <AvatarCircles numPeople={99} avatarUrls={avatars} />;
+                </div>
+            </div>
+
+            {/* orange */}
+            <div
+                className="h-full w-full absolute bottom-0"
+                style={{ background: "radial-gradient(150% 100% at 50% 20%, #000 50%, rgba(255, 255, 100, 1) 100%)" }}
+            ></div>
+
+            {/* yellow */}
+            <div
+                className="h-full w-full absolute bottom-0 z-10 mix-blend-overlay"
+                style={{ background: "radial-gradient(120% 100% at 50% 10%, #000 50%, rgba(255, 140, 50, 1) 100%)" }}
+            ></div>
+
+            {/* white */}
+            <div
+                className="h-full w-full absolute bottom-0 z-20 mix-blend-overlay"
+                style={{ background: "radial-gradient(90% 100% at 50% 20%, #000 50%, rgba(255, 255, 255, 1) 100%)" }}
+            ></div>
         </motion.div>
     )
 }
---
+++ src/components/Homepage/data.tsx
@@ -0,0 +1,40 @@
+import { LucideAirVent } from 'lucide-react';
+
+export const avatars = [
+    {
+        imageUrl: "https://avatars.githubusercontent.com/u/16860528",
+        profileUrl: "https://github.com/dillionverma",
+    },
+    {
+        imageUrl: "https://avatars.githubusercontent.com/u/20110627",
+        profileUrl: "https://github.com/tomonarifeehan",
+    },
+    {
+        imageUrl: "https://avatars.githubusercontent.com/u/106103625",
+        profileUrl: "https://github.com/BankkRoll",
+    },
+    {
+        imageUrl: "https://avatars.githubusercontent.com/u/59228569",
+        profileUrl: "https://github.com/safethecode",
+    },
+    {
+        imageUrl: "https://avatars.githubusercontent.com/u/59442788",
+        profileUrl: "https://github.com/sanjay-mali",
+    },
+    {
+        imageUrl: "https://avatars.githubusercontent.com/u/89768406",
+        profileUrl: "https://github.com/itsarghyadas",
+    },
+];
+
+export const leftLogos = [
+    { key: 1, logo: <LucideAirVent />, rotate: "rotate-20", position: "right-10" },
+    { key: 2, logo: <LucideAirVent />, rotate: "-rotate-20", position: "-right-10" },
+    { key: 3, logo: <LucideAirVent />, rotate: "rotate-20", position: "right-10" },
+];
+
+export const rightLogos = [
+    { key: 1, logo: <LucideAirVent />, rotate: "rotate-20", position: "-right-10" },
+    { key: 2, logo: <LucideAirVent />, rotate: "-rotate-20", position: "right-10" },
+    { key: 3, logo: <LucideAirVent />, rotate: "rotate-20", position: "-right-10" },
+];

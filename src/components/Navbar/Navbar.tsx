8d53e0fa3ffe89e5026fcada042c5c9222fe79c8
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-03-11 03:37:51 +0530

    minor change

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import * as motion from "motion/react-client"
 
 const Navbar = () => (
     <motion.div
-        className='w-2/3 h-16 fixed z-60 top-5 flex justify-between items-center rounded-2xl p-[1px] bg-gradient-to-br from-[#FFD700]/70 to-[#FFD700]/20'
+        className='w-2/3 h-16 fixed z-60 top-5 flex justify-between items-center rounded-2xl p-[1px] bg-gradient-to-br from-[#FF8C32] via-[#EFBF04]/50 to-transparent'
         initial={{ y: -100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5 }}

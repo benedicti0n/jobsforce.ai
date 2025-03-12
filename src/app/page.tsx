69b10540cf7f269c2bed69d12f3b18e53c4818d2
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-03-12 11:52:15 +0530

    feat: add Radix UI components (Accordion, Checkbox, Label, Input) and update homepage with Navbar1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import ToolsBentoPage from "@/components/ToolsBentoPage/ToolsBentoPage";
 import FAQ from "@/components/FAQ/FAQ";
 import Footer from "@/components/Footer/Footer";
 import ProcessPage from "@/components/ProcessPage/ProcessPage";
+import { Navbar1 } from "@/components/blocks/shadcnblocks-com-navbar1";
 // import Navbar2 from "@/components/Navbar/Navbar2";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full">
       {/* <Navbar2 /> */}
+      <Navbar1 />
       <Homepage />
       <ToolsBentoPage />
       <ProcessPage />
